e125ab4039fd573c52a2d7f5c276ffa212d502cb
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-07-13 09:13:07 +0530

    Updated SecurityConfig.java

--- Project-for-Medical/src/main/java/com/medicare/ProjectforMedical/config/SecurityConfig.java
+++ Project-for-Medical/src/main/java/com/medicare/ProjectforMedical/config/SecurityConfig.java
@@ -3,14 +3,33 @@ package com.medicare.ProjectforMedical.config;
 import com.medicare.ProjectforMedical.security.AuthenticationFilter;
 import com.medicare.ProjectforMedical.security.JWTAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JWTAuthenticationEntryPoint entryPoint;
+    private final JWTAuthenticationEntryPoint point;
 
     private final AuthenticationFilter filter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests().
+                requestMatchers("/api").authenticated().requestMatchers("/api/user/add,api/doctor/add").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 
 }
